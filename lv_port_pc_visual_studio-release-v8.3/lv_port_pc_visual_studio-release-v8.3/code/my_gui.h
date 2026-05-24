6e172f6aa77bc813db7f5d6a52f381acf43abba1
lei <lmh19104@outlook,com>
2026-05-25 00:17:22 +0800

    合并远程更改

---
+++ lv_port_pc_visual_studio-release-v8.3/lv_port_pc_visual_studio-release-v8.3/code/my_gui.h
@@ -0,0 +1,17 @@
+﻿#ifndef _MY_GUI_H
+#define _MY_GUI_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+void mygui(void);
+
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_DEMO_WIDGETS_H*/
